cf731a9f5846b37e10b504f0ad46ad97736eddb1
pikax <david-181@hotmail.com>
2017-09-28 15:07:20 +0100

    bump 0.3.1

--- dist/parser.d.ts
+++ dist/parser.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="cheerio" />
-import { Collection, Guild, Profile, ShipCollection } from "./interface";
+import { Collection, Guild, Mod, Profile, ShipCollection } from "./interface";
 export declare function parseCollection($: CheerioStatic): Collection;
 export declare function parseProfile($: CheerioStatic): Profile;
 export declare function parseGuild($: CheerioStatic): Guild;
 export declare function parseShips($: CheerioStatic): ShipCollection;
+export declare const parseModCollection: ($: CheerioStatic) => Mod[];
